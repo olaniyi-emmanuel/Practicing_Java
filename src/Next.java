116cee8d1614e5593218ca92aa3bcc6e76201255
emmanuel-olafusi <emmanuelolafusi.digifyng@gmail.com>
2023-05-21 00:04:09 +0100

    Read the user's favourite number (doble, int,...) and then display it in a message of choice

--- src/Next.java
+++ src/Next.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Next {
+    //Read the user's favourite number (doble, int,...) and then display it in a message of choice
     public static void main(String[] args) {
+        Scanner userFavouriteNumber = new Scanner(System.in);
+        System.out.print("Enter your favourite number ");
+        System.out.println("Your favourite number is " + userFavouriteNumber.nextDouble());
 
     }
 
