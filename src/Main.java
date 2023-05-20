1968b14bd593a968fbbb4df66d5cf49b1107e0b5
emmanuel-olafusi <emmanuelolafusi.digifyng@gmail.com>
2023-05-20 23:19:54 +0100

    Getting input from user

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner userFullName = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        System.out.println("Your name is " + userFullName.nextLine());
+
+        //Taking the age of the user
+
+        Scanner userAge = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int newUserAge =userAge.nextInt();
+        System.out.println("You are  " + newUserAge + "years old");
+
+        //Taking the gender of the user
+        Scanner userGender = new Scanner(System.in);
+        System.out.print("Enter your Gender Male_or_Female: ");
+        System.out.println("Your chosen gender is " + userGender.nextLine());
+
+
+
+
+    }
+
+
+}
